56520f92f3631949da60f55462df86d0f61a8936
Antoine <arocq92@gmail.com>
2025-01-01 15:16:22 +0100

    selection d'upgrade random fonctionnelle

--- Assets/Bolt 2D LittleWars VE3/MyScripts/UpgradesScript.cs
+++ Assets/Bolt 2D LittleWars VE3/MyScripts/UpgradesScript.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngin.UI;
 //using System.Linq;
+
+// Text s'update dans le trigger après la fonction d'obtention d'upgrade --> le bouton onlick est la fonction crée pour faire l'effet de l'upgrade : longue fonction avec bcp de if
 
 public class UpgradesScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    //public  GameObject mainCharacter;
+    public  GameObject mainCharacter;
     //public  Script mainCharacterScript;
-    //public Text UIText;
     public Upgrade upgradeTemp;
     public string descriptionTemp;
     public string nomUpgradeRandom;
@@ -49,11 +51,11 @@ public class UpgradesScript : MonoBehaviour
         }   
     }
     public List<Upgrade> upgrades= new List<Upgrade>{
-        new Upgrade("Amélioration de la vitesse", 1, 20, "Augmente la vitesse du joueur"),
-        new Upgrade("Amélioration de la hauteur du saut", 2, 20, "Augmente la hauteur du saut du joueur"),
-        new Upgrade("Tu peux spam les flèches frérot", 3, 5, "Permet de spam les flèches"),
-        new Upgrade("Tu peux spam les spells", 4, 5, "Permet de spam les spells"),
-        new Upgrade("Augmentation de l'attaque", 5, 50, "Augmente l'attaque du joueur")};
+        new Upgrade("Amélioration de la vitesse", 0, 20, "Augmente la vitesse du joueur"),
+        new Upgrade("Amélioration de la hauteur du saut", 1, 20, "Augmente la hauteur du saut du joueur"),
+        new Upgrade("Tu peux spam les flèches frérot", 2, 5, "Permet de spam les flèches"),
+        new Upgrade("Tu peux spam les spells", 3, 5, "Permet de spam les spells"),
+        new Upgrade("Augmentation de l'attaque", 4, 50, "Augmente l'attaque du joueur")};
     void Start()
     {
         //nombreRandom = Random.Range(0, 100);
@@ -66,6 +68,7 @@ public class UpgradesScript : MonoBehaviour
     }
     Upgrade ActiveUpgrade(List<Upgrade> upgrade){
         indiceTemp = SelectUpgrade(upgrades);
+        Debug.Log("Indice sélectionné dans ACTIVE :" + indiceTemp);
         for (int i=0; i<upgrades.Count; i++){
             if (indiceTemp == upgrade[i].id){
                 upgradeTemp = upgrade[i];
@@ -75,11 +78,13 @@ public class UpgradesScript : MonoBehaviour
         return upgradeTemp;
         
     }
+    void UpdateText(){
+
+    }
     void UpdateText(Upgrade upgrade /*, UItext text*/){
         //UIText.text = upgrade.description;
     }
     void EffetUpgrade(Upgrade upgrade){
-        Debug.Log("Effet");
         switch (upgrade.id){
             case 1:
                 Debug.Log("Vitesse");
@@ -111,7 +116,12 @@ public class UpgradesScript : MonoBehaviour
         if (collider.gameObject.tag == "Player"){
             Destroy(gameObject);
             upgradeTemp = ActiveUpgrade(upgrades);
-            EffetUpgrade(upgradeTemp);
+            if(upgradeTemp == null){
+                Debug.Log("Erreur : Upgrade null");
+            } else {
+                 EffetUpgrade(upgradeTemp);
+                 UpdateText(upgradeTemp.descriptif);            
+            }
             //EffetUpgrade();
         }
     }
@@ -121,18 +131,21 @@ public class UpgradesScript : MonoBehaviour
         upgrades[upgrades.Length - 1] = new upgrade("Augmentation du Mana max", 6, 40, "Augmente les points de mana maximum du joueur");*/
     }
     int SelectUpgrade(List<Upgrade> upgrade){
+        int i = 0;
+        int iMax = upgrade.Count;
         nombreRandom = Random.Range(0, 100);
-        Debug.Log("nombre random" + nombreRandom);
-        while (nombreRandom > 0){ // Faire en sorte que 1 itération = 1 - nombre random et pas après itération
-            Debug.Log("nombre random après itération" + nombreRandom);
-            for (int i=0; i< upgrade.Count; i++){
-                Debug.Log("indice :" + i + "upgrade :" + upgrade[i].nom);
-                //idTemp = upgrades[i].id;
-                indiceTemp = i;
-                nombreRandom -= upgrade[i].proba;
-            }
-        }
-        Debug.Log("indice temp après fonction" + indiceTemp);
-        return indiceTemp;
+        Debug.Log("Nombre random :" + nombreRandom);
+                while (i < iMax){
+                    //Debug.Log("indice :" + i + "upgrade :" + upgrade[i].nom);
+                    //idTemp = upgrades[i].id;
+                    //indiceTemp = i;
+                    nombreRandom -= upgrade[i].proba;
+                    if(nombreRandom <= 0){
+                        Debug.Log("Indice sélectionné :" + (i));
+                        return i;
+                    }
+                    i+=1;
+                }
+        return 0;
     }
 }
